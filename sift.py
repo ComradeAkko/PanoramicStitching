bd75b755052123241d3ab07863077d84fdf730aa
ComradeAkko <jirojpn2412@gmail.com>
2020-09-23 19:22:15 +0900

    finished finding qualified keypoints

--- sift.py
+++ sift.py
@@ -14,12 +14,90 @@ def ssExtremaDetect(imgPath):
     imgs = gaussPyramid(img, 4, 3, 0.5)
     imgs = diffGaussPyramid(imgs)
     candidates = candidatePyramid(imgs)
+    keypoints = findPyramidKeypoints(candidates, imgs)
 
 # returns qualified keypoints that are localized, not low contrast, and not on an edge
 def findPyramidKeypoints(candidates, pyramid):
     pyraKeys = []
+    for i in range(len(pyramid)):
+        pyraKeys.append(findOctaveKeypoints(candidates[i], pyramid[i]))
 
     return pyraKeys
+
+# returns the qualified keypoints of an octave by
+# offsetting it, checking for low contrast, and whether it's on the edge
+
+# heavy guidance from the original paper and https://dsp.stackexchange.com/questions/10403/sift-taylor-expansion
+# for additional explanations
+def findOctaveKeypoints(octCandidates, oct):
+    keypoints = []
+
+    # cycle through the candidates of the octave
+    for cand in octCandidates:
+        x = cand[0]
+        y = cand[1]
+        z = cand[2]
+
+        imgRow, imgCol = oct[0].shape 
+
+        outofBounds = False
+
+        # iterate at most five times to try to converge to an offset
+        for i in range(5):
+            # calculate the first derivatives and 2nd derivatives of the candidate
+            dx = (oct[z][x+1,y] - oct[z][x-1,y])/2
+            dy = (oct[z][x,y+1] - oct[z][x,y-1])/2
+            dz = (oct[z+1][x,y] - oct[z-1][x,y])/2
+
+            dxx = oct[z][x+1,y] - 2*oct[z][x,y] + oct[z][x-1,y]
+            dxy = ((oct[z][x+1,y+1] - oct[z][x-1,y+1]) - (oct[z][x+1,y-1] - oct[z][x-1,y-1]))/4
+            dyy = oct[z][x,y+1] - 2*oct[z][x,y] + oct[z][x,y-1]
+            dyz = ((oct[z+1][x,y+1] - oct[z+1][x,y-1]) - (oct[z-1][x,y+1] - oct[z-1][x,y-1]))/4
+            dzz = oct[z+1][x,y] - 2*oct[z][x,y] + oct[z-1][x,y]
+            dxz = ((oct[z+1][x+1,y] - oct[z+1][x-1,y]) - (oct[z-1][x+1,y] - oct[z-1][x-1,y]))/4
+
+            # construct gradient and hessian
+            grad = np.array([dx,dy,dz])
+            hessian = np.array([[dxx,dxy,dxz],[dxy,dyy,dyz],[dxz,dyz,dzz]])
+
+            # calculate offset
+            offset = -np.linalg.lstsq(hessian, grad, rcond=None)[0]
+
+            # if the offset is less than 0.5 in all dimensions, break because the keypoint is ok
+            if offset[0] < 0.5 and offset[1] < 0.5 and offset[2] < 0.5:
+                break
+            
+            # update the keypoint position
+            x += int(round(offset[0]))
+            y += int(round(offset[1]))
+            z += int(round(offset[2]))
+
+            # if the offset is out of bounds, skip it
+            if x < 0 or x >= imgRow-1 or y < 0 or y >= imgCol-1 or z < 1 or z >= len(oct)-1:
+                outofBounds = True
+                break
+        
+        # if the offset isn't out of bounds continue to further checks
+        if not outofBounds:
+            # get the function value at the extrenum and check for low contrast
+            funcV = oct[z][x,y] + np.dot(grad, offset)/2
+            
+            # if the function value is greater than or equal to 0.03, check to see if point is on edge
+            if funcV >= 0.03:
+                # calculate the trace and determinant based on 2x2 Hessian
+                trace = dxx + dyy
+                det = dxx*dyy - dxy**2
+
+                # if the determinant is positive, continue (non-positive means det is point is not extrenum
+                # as the curvature has different sides)
+                if det > 0:
+                    # check the ratio principal curvature to see if it is below a threshold (not an edge)
+                    if trace**2/det < (10+1)**2/10:
+                        
+                        # add keypoint if it has cleared all conditions
+                        keypoints.append([x,y,z])
+
+    return keypoints
 
 # returns the total keypoint candidates of the DoG pyramid
 def candidatePyramid(pyramid):
@@ -35,6 +113,7 @@ def candidateOctave(oct):
     for i in range(1,len(oct)-1):
         for j in range(1, imgRow-1):
             for k in range(1, imgCol-1):
+                # get the maximums and minimums of the above, lower and current layers
                 aboveMx = np.max(oct[i+1][j-1:j+2, k-1:k+2])
                 aboveMn = np.min(oct[i+1][j-1:j+2, k-1:k+2])
                 belowMx = np.max(oct[i-1][j-1:j+2, k-1:k+2])
@@ -43,8 +122,9 @@ def candidateOctave(oct):
                 currMx =  np.max(oct[i][j-1:j+2, k-1:k+2])
                 currMn =  np.min(oct[i][j-1:j+2, k-1:k+2])
 
+                # if the center pixel is the unchallenged maximum/minimum in above, lower and current layers, add it to candidates
                 if max([aboveMx, belowMx, currMx]) == oct[i][j,k] or min([aboveMn, belowMn, currMn]) == oct[i][j,k]:
-                    candidates.append([i,j,k])
+                    candidates.append([j,k,i])
     return candidates
     
 # calculate the difference of gaussians in each octave
@@ -55,7 +135,6 @@ def diffGaussPyramid(pyramid):
     for i in range(len(pyramid)):
         oct = pyramid[i]
         diffOct = []
-        print(len(oct))
         for j in range(len(oct)-1):
             diffOct.append(oct[j] - oct[j+1])
         diffPyramid.append(diffOct)
